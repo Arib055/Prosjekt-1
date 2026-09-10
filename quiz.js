130689675490996a071b14a42e6e0e9fa1b24cef
Arib055 <arwaa006@osloskolen.no>
2026-09-10 14:10:15 +0200

    nettside quiz

--- quiz.js
+++ quiz.js
@@ -1,7 +1,15 @@
-var questions = {
-    queston1: "Hva driver "
-}
+var questions = [
+     "Hvor mange bits står en MAC-adresse av?",
+     "Hvor mange bits er en byte?",
+     "?",
+     "?"
+]
 
 function runQuiz() {
     console.log('')
 }
+
+const question = document.getElementById("question")
+
+question.textContent = questions[0]
+
